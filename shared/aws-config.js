db724bd31b59b3d6f3ec20bba24a4f0602dce7cc
Yuying Zhang <38091965+ZenobiaZhang@users.noreply.github.com>
2019-07-07 07:28:50 -0400

    change aws config

--- shared/aws-config.js
+++ shared/aws-config.js
@@ -1,14 +1,14 @@
 var AWS = require("aws-sdk");
 
 AWS.config.update({
-  region: "eu-west-1",
+  region: "us-east-1",
   endpoint: "http://localhost:8000" // http://localhost:8000 or https://dynamodb.eu-west-1.amazonaws.com
 });
 
 
 var awsconfig = {
   conf: {
-    region: "eu-west-1",
+    region: "us-east-1",
     endpoint: "http://localhost:8000" // http://localhost:8000 or https://dynamodb.eu-west-1.amazonaws.com
   },
   docClient: new AWS.DynamoDB.DocumentClient(),
